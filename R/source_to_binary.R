ad7986ce8aa70d215ddc94f2f3c53e7dbe7930db
Richard White <w@rwhite.no>
2020-02-18 16:30:43 +0100

    include repos as option

--- R/source_to_binary.R
+++ R/source_to_binary.R
@@ -1,8 +1,13 @@
 #' source_to_binary
 #' @param packages_to_build a
 #' @param drat_repo a
+#' @param repos a
 #' @export
-source_to_binary <- function(packages_to_build, drat_repo) {
+source_to_binary <- function(
+  packages_to_build,
+  drat_repo,
+  repos = c("https://cran.rstudio.com")
+  ) {
   drat::pruneRepo(drat_repo, type = "source", remove = TRUE)
   drat::pruneRepo(drat_repo, type = "win.binary", remove = TRUE)
 
@@ -15,7 +20,13 @@ source_to_binary <- function(packages_to_build, drat_repo) {
   pkg_with_versions <- gsub(".tar.gz$", "", pkg_srcs)
 
   for (i in seq_along(pkg_srcs)) {
-    devtools::install_local(file.path(drat_repo, "src", "contrib", pkg_srcs[i]), repos = "https://cran.rstudio.com", force = T, upgrade = "always")
+    devtools::install_local(
+      file.path(drat_repo, "src", "contrib", pkg_srcs[i]),
+      repos = "https://cran.rstudio.com",
+      force = T,
+      upgrade = "always"
+    )
+
     pkg_binary <- devtools::build(
       pkg = file.path(drat_repo, "src", "contrib", pkg_srcs[i]),
       path = tempdir(),
