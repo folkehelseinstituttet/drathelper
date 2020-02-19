7847e41c0b7e380392594f37b69918caa278d3eb
Travis CI User <travis@example.org>
2020-02-19 09:20:06 +0000

    Travis CI update 7 [ci skip]

--- R/3_onAttach.R
+++ R/3_onAttach.R
@@ -1,5 +1,5 @@
 .onAttach <- function(libname, pkgname) {
   packageStartupMessage("PACKAGE: drathelper")
-  packageStartupMessage("Version 2020.02.19 at 07:56")
+  packageStartupMessage("Version 2020.02.19 at 09:06")
   packageStartupMessage("Developed by Richard White, Norwegian Institute of Public Health")
 }
--- R/get_suggests.R
+++ R/get_suggests.R
@@ -1,9 +1,9 @@
 #' get_suggests
 #' @param package a
 #' @export
-get_suggests <- function(package){
-  d <- data.frame(read.dcf(file = system.file("DESCRIPTION",package=package)))$Suggests
-  d <- stringr::str_replace_all(d,"\n"," ")
-  d <- stringr::str_split(d,", ")[[1]]
+get_suggests <- function(package) {
+  d <- data.frame(read.dcf(file = system.file("DESCRIPTION", package = package)))$Suggests
+  d <- stringr::str_replace_all(d, "\n", " ")
+  d <- stringr::str_split(d, ", ")[[1]]
   d
 }
