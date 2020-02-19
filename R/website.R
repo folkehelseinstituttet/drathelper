54d0613564c8cddfb113ed05e1abdbff14cbcb55
Richard White <w@rwhite.no>
2020-02-19 08:54:49 +0100

    Code to create website

---
+++ R/website.R
@@ -0,0 +1,196 @@
+# miniCRAN::
+#   utils::
+
+drat_repo <- "/git/drat"
+
+win_version <- c("3.6")
+
+get_description_src <- function(drat_repo, pkg, version){
+  utils::untar(
+    file.path(drat_repo,"src","contrib",paste0(pkg,"_",version,".tar.gz")),
+    files = file.path(pkg,"DESCRIPTION"),
+    exdir = tempdir()
+  )
+  x <- data.table::data.table(read.dcf(file = file.path(tempdir(),pkg,"DESCRIPTION")))
+  unlink(file.path(tempdir(),pkg))
+  x
+}
+
+get_description_win <- function(drat_repo, pkg, version){
+  utils::unzip(
+    file.path(drat_repo,"bin","windows","contrib",win_version,paste0(pkg,"_",version,".zip")),
+    files = file.path(pkg,"DESCRIPTION"),
+    exdir = tempdir()
+  )
+  x <- data.table::data.table(read.dcf(file = file.path(tempdir(),pkg,"DESCRIPTION")))
+  unlink(file.path(tempdir(),pkg))
+  x
+}
+
+#' get_package_table
+#' @param drat_repo a
+#' @param win_version a
+#' @export
+get_package_table <- function(drat_repo, win_version){
+  pkgs_src <- data.table::data.table(read.dcf(file.path(drat_repo,"src","contrib","PACKAGES")))
+  pkgs_win <- data.table::data.table(read.dcf(file.path(drat_repo,"bin","windows","contrib",win_version,"PACKAGES")))
+  pkgs_cran <- data.table::data.table(miniCRAN::getCranDescription(pkgs_src$Package))
+  pkgs_cran <- pkgs_cran[pkgs_cran$Package %in% pkgs_src$Package,]
+
+  res <- vector("list", length=nrow(pkgs_src))
+  for(i in seq_along(res)){
+    pkg <- pkgs_src$Package[i]
+
+    desc_src <- get_description_src(
+      drat_repo = drat_repo,
+      pkg = pkg,
+      version = pkgs_src[pkgs_src$Package==pkg,]$Version
+    )
+
+    retval <- data.frame(
+      Package = desc_src$Package,
+      Source = desc_src$Version,
+      Windows = NA,
+      CRAN = glue::glue(NA),
+      Title = desc_src$Title
+    )
+
+    if(pkg %in% pkgs_win$Package){
+      desc_win <- get_description_src(
+        drat_repo = drat_repo,
+        pkg = pkg,
+        version = pkgs_src[pkgs_src$Package==pkg,]$Version
+      )
+      retval$Windows <- desc_win$Version
+    }
+
+    if(pkg %in% pkgs_cran$Package){
+      desc_cran <- pkgs_cran[pkgs_cran$Package==pkg,]
+      retval$CRAN <- glue::glue("<a href='https://cran.r-project.org/web/packages/{pkg}/index.html'>{desc_cran$Version}</a>")
+    }
+    res[[i]] <- retval
+  }
+
+  res <- data.table::rbindlist(res)
+
+  ht <- huxtable::hux(res, add_colnames = T)
+  ht <- huxtable::theme_article(ht)
+  huxtable::escape_contents(ht)[,4] <- FALSE
+  ht
+}
+
+#' create_website_index
+#' @param drat_repo a
+#' @param win_version a
+#' @param output_dir a
+#' @export
+create_website_index <- function(drat_repo, win_version, output_dir){
+  rmarkdown::render(
+    system.file("extdata","index.Rmd",package="drathelper"),
+    output_dir = output_dir,
+    params = list(
+      drat_repo = drat_repo,
+      win_version = win_version
+    )
+  )
+}
+
+refer_to_other_packages <- function(val,pkgs_src){
+  if(!is.na(val)){
+    val <- stringr::str_replace_all(val,"\\n"," ")
+    val <- stringr::str_split(val, ", ")[[1]]
+    val[val %in% pkgs_src$Package] <- paste0(
+      "<a href='",
+      val[val %in% pkgs_src$Package],
+      ".html'>",
+      val[val %in% pkgs_src$Package],
+      "</a>"
+      )
+    val <- paste0(val, collapse=", ")
+  }
+  return(val)
+}
+
+#' create_website_packages
+#' @param drat_repo a
+#' @param win_version a
+#' @param pkgdown_base_url a
+#' @export
+create_website_packages <- function(drat_repo, win_version, pkgdown_base_url){
+  dir.create(file.path(drat_repo,"packages"), showWarnings = F)
+
+  pkgs_src <- data.table::data.table(read.dcf(file.path(drat_repo,"src","contrib","PACKAGES")))
+  pkgs_win <- data.table::data.table(read.dcf(file.path(drat_repo,"bin","windows","contrib",win_version,"PACKAGES")))
+  pkgs_cran <- data.table::data.table(miniCRAN::getCranDescription(pkgs_src$Package))
+  pkgs_cran <- pkgs_cran[pkgs_cran$Package %in% pkgs_src$Package,]
+
+  for(pkg in pkgs_src$Package){
+
+    desc_src <- get_description_src(
+      drat_repo = drat_repo,
+      pkg = pkg,
+      version = pkgs_src[pkgs_src$Package==pkg,]$Version
+    )
+    desc_src$Depends <- refer_to_other_packages(
+      val = desc_src$Depends,
+      pkgs_src = pkgs_src
+    )
+    desc_src$Imports <- refer_to_other_packages(
+      val = desc_src$Imports,
+      pkgs_src = pkgs_src
+    )
+    desc_src$Suggests <- refer_to_other_packages(
+      val = desc_src$Suggests,
+      pkgs_src = pkgs_src
+    )
+
+    vers_win <- NA
+    if(pkg %in% pkgs_win$Package){
+      desc_win <- get_description_src(
+        drat_repo = drat_repo,
+        pkg = pkg,
+        version = pkgs_src[pkgs_src$Package==pkg,]$Version
+      )
+      vers_win <- desc_win$Version
+    }
+
+    vers_cran <- NA
+    if(pkg %in% pkgs_cran$Package){
+      desc_cran <- pkgs_cran[pkgs_cran$Package==pkg,]
+      vers_cran <- desc_cran$Version
+    }
+
+    pkgdown_url <- file.path(pkgdown_base_url,pkg)
+    pkgdown_url <- gsub("//","/",pkgdown_url)
+    if(!httr::http_error(pkgdown_url, followlocation=0L)){
+      doc <- data.frame("Documentation",pkgdown_url)
+    } else {
+      doc <- NULL
+    }
+
+    tab <- data.table::rbindlist(list(
+      doc,
+      data.frame("Version (source)",desc_src$Version),
+      data.frame("Version (windows)",vers_win),
+      data.frame("Version (CRAN)",vers_cran),
+      data.frame("Depends",desc_src$Depends),
+      data.frame("Imports",desc_src$Imports),
+      data.frame("Suggests",desc_src$Suggests),
+      data.frame("Author",desc_src$Author),
+      data.frame("Maintainer",desc_src$Maintainer),
+      data.frame("License",desc_src$License),
+      data.frame("Needs complication",desc_src$NeedsCompilation)
+    ),use.names=F)
+
+
+    rmarkdown::render(
+      system.file("extdata","package.Rmd",package="drathelper"),
+      output_dir = file.path(drat_repo,"packages"),
+      output_file = paste0(desc_src$Package,".html"),
+      params = list(
+        desc_src = desc_src,
+        tab = tab
+      )
+    )
+  }
+}
